ee170d363bcd3c7c491a007a0b52d24451bc5eb3
zach <zach@wayfaring.io>
2017-08-27 20:41:30 -0700

    Finalized AU BU weighted scoring and function for rating

--- survey_poc2.R
+++ survey_poc2.R
@@ -1,14 +1,16 @@
+################################################################################
 # Author: Zach                                                                 #
-# Description of Script Purpose:                                               #
-# This script automates the processing of tidy data in Excel workbooks. It eli-#
-# minates moving all of the spreadsheets into a directory by manually copying  #
-# and pasting all of the content into a semi-static worksheet. The semi-static #
-# worksheet approach is subject to errors or ommissions if the underlying orig-#
-# inal worksheets are modified at a later date. As a result, the underlying wo-#
-# rkbooks can continue to be modified and the script just needs to be run once #
-# after all modifications are completed. The script performs various grouping  #
-# actions and then summarizes the raw scores by producing simple averages and  #
-# the corresponding ratings.                                                   #
+# License: MIT, free to use, re-distrubte and modify w/o commercial gain.      #
+# Date: ~ 20 Aug 2017                                                          #
+# Purpose: # This script automates the processing of tidy data in Excel workbo #
+#oks. It eliminates moving all of the spreadsheets into a directory by manual- #
+#ly copying and pasting all of the content into a semi-static worksheet.       #
+# The semi-static worksheet approach is subject to errors or ommissions if the #
+#underlying original worksheets are modified at a later date. As a result, the #
+#underlying workbooks can continue to be modified and the script just needs to #
+# be run once after all modifications are completed. The script performs vari- #
+#ous grouping actions and then summarizes the raw scores by producing simple   #
+#averages and the corresponding ratings.                                       #
 ################################################################################
 
 #load packages
@@ -18,13 +20,15 @@ require(readxl) #fast XSLX parser
 # Establish the file names to read and read the contents of the Excel files into
 # a list of data.frames
 files.list <-list.files(pattern = '*.xlsx')  # Get a list of file names in the directory
-df.files <-setNames(lapply(files.list, read_excel), files.list)  # Read the contents of those files into a list of data.frames
-df <- bind_rows(df.files, .id = "file_src")
+df.files <-setNames(lapply(files.list, read_excel), files.list)  # Write the 
+# contents of those files into a list of data.frames
+df <- bind_rows(df.files, .id = "file_src") #bind the rows together to form a 
+# tall data set of tidy data, with every variable with an observation.
 
 # Tidy up yo-self! And don't leave no trash!
-#rm(df.files, files.list)
+rm(df.files, files.list)
 
-# Begin Assessment Unit and Business Unit Summary Statistics ###################
+# Create Assessment Unit and Business Unit Summary Statistics ##################
 # Stage 2 processing                                                           #
 # Create summary statistics (simple average) data.frames at the assessment and #
 # business unit levels across the control categories. This section of the      #
@@ -35,24 +39,30 @@ df <- bind_rows(df.files, .id = "file_src")
 # corresponding scores for the given category (GOV, CORC, etc scores [0-4])    #
 # where 0 = N/A, 1 = "Highly Effective", 2 = "Generally Effective",            #
 # 3 = "Marginally Effective" and # 4 = "Ineffective". These scores and ratings #
-# are established and defined in the methodology.                              #
+# are established and defined in the methodology. Subsequent sections handle   #
+# the weighted factorization of the scores and the sum of weighted scores.     #
 ################################################################################
 
-# This section produces a table of raw average scores for each business unit
-# and each assessment unit across the respective control categories.
+# This section produces a table of simple averages (scores) for each business
+# and each assessment unit across the respective control categories for each 
+# level of the unit AU vs BU.
 scores.list <- list(AU = df %>%
+                  # scores.list$AU
                   group_by(AU, CC) %>% # groups df by AU and CC
-                  summarise(avg = mean(Score, na.rm = TRUE)),
-                
+                  summarise(avg = mean(Score, na.rm = TRUE)), #AU means
+              
+                      # scores.list$BU
                   BU = df %>% # 
                   group_by(BU, CC) %>% # groups df by BU and CC
-                  summarise(avg = mean(Score, na.rm = TRUE))) #end-list
+                  summarise(avg = mean(Score, na.rm = TRUE)) # BU means
+              ) #end-list
 
 df.table <- bind_rows(scores.list, .id = "Unit_Level") %>% 
   replace(., is.na(.), "") %>%  # To avoid "NA" values when we "unite" below
   unite(Unit_Name, AU, BU, sep="") %>% 
   spread(CC, avg)
-df.table
+# df.table
+rm(df) # clean up space
 #
 # End Stage 2 Processing #######################################################
 
@@ -92,14 +102,16 @@ convert_scores <- function(score) {
 au.ratings.tbl <- df.table %>%
   filter(Unit_Level == "AU") %>%
   mutate_at(c("AUDIT", "CORC", "GOV", "PPS", "TMSC", "TRAIN"), convert_scores)
-  au.ratings.tbl[,1:8]
+#  au.ratings.tbl[,1:8]
 
 bu.ratings.tbl <- df.table %>%
   filter(Unit_Level == "BU") %>%
   mutate_at(c("AUDIT", "CORC", "GOV", "PPS", "TMSC", "TRAIN"), convert_scores)
-bu.ratings.tbl[,1:8]
+#  bu.ratings.tbl[,1:8]
 
-# calculate weighted scores for the lines of business
+# Calculate weighted scores for the assessment units. Matrix multiplication occ-
+# urs in an element-wise nature for the two given data.frames. A raw score
+# multiplied by its associated weight in the weight.table df.
 au.names <- c("BSA_Admin", "BSG", "CBD", "CBG", "WMG")
 au.weighted.scores <- spread(scores.list$AU, CC, avg)[,-1]*weight.table[,-1]
 au.weighted.scores <- cbind(AU = paste0(au.names), au.weighted.scores)
@@ -107,49 +119,25 @@ au.weighted.scores <- cbind(AU = paste0(au.names), au.weighted.scores)
 lob.weighted.scores <- au.weighted.scores %>%
   gather(Control_Category, weighted.score, -AU) %>%
   group_by(Control_Category) %>%
-  summarise(CBD = round(weighted.mean(weighted.score, c(1,1,1,0,0)),2),
+  summarise(
+    # Order: BSA_Admin, BSG, CBD, CBG, WMG
+            CBD = round(weighted.mean(weighted.score, c(1,1,1,0,0)),2),
             CBG = round(weighted.mean(weighted.score, c(1,1,0,1,0)),2),
-            WMG = round(weighted.mean(weighted.score, c(1,1,0,0,1)),2)) %>%
+            WMG = round(weighted.mean(weighted.score, c(1,1,0,0,1)),2), 
+            BSA_Admin = round(weighted.mean(weighted.score, c(1,0,0,0,0)),2),
+            BSG = round(weighted.mean(weighted.score, c(0,1,0,0,0)),2)) %>%
   ungroup()
 lob.weighted.scores
 
 # rearrange result & calculate overall sum for each line of business
 lob.control.results <- lob.weighted.scores %>%
-  gather(LOB, score, -Control_Category) %>%
+  gather(AU, score, -Control_Category) %>%
   spread(Control_Category, score) %>%
-  select(LOB, GOV, CORC, TMSC, AUDIT, PPS, TRAIN) %>%
-  mutate(Control_Score = AUDIT + CORC + GOV + PPS + TMSC + TRAIN) %>%
-  mutate(Control_Rating = case_when(
-    Control_Score > 3.499 ~ "Ineffective",
-    Control_Score > 2.499 & Control_Score <= 3.499 ~ "Marginally Effective",
-    Control_Score >= 1.5 & Control_Score <= 2.499 ~ "Generally Effective",
-    Control_Score < 1.5 ~ "Highly Effective"
-  )
-)# end-chain
+  select(AU, GOV, CORC, TMSC, AUDIT, PPS, TRAIN) %>%
+  mutate(Overall_Score = AUDIT + CORC + GOV + PPS + TMSC + TRAIN) %>%
+  mutate(Overall_Rating = convert_scores(Overall_Score)
+) # end-chain
 lob.control.results
 
 # calculate weighted scores for BSA Admin and BSG
-central.control.scores <- au.weighted.scores[,-1]*weight.table[,-1]
-bsa.bsg.weighted.scores$AU <- au.scores$AU
-
-# calculate scores for BSA Admin and BSG
-bsa.bsg.weighted.scores <- bsa.bsg.weighted.scores %>%
-  gather(Control_Category, weighted.score, -AU) %>%
-  group_by(Control_Category) %>%
-  summarise(BSA_Admin = weighted.mean(weighted.score, c(1,0,0,0,0)),
-            BSG = weighted.mean(weighted.score, c(0,1,0,0,0))) %>%
-  ungroup()
-
-# rearrange result & calculate overall sum for BSA Admin and BSG
-bsa.bsg.control.scores <- bsa.bsg.weighted.scores %>%
-  gather(group, score, -Control_Category) %>%
-  spread(Control_Category, score) %>%
-  select(group, GOV, CORC, TMSC, AUDIT, PPS, TRAIN) %>%
-  mutate(Control_Score = GOV + CORC + TMSC + AUDIT + PPS + TRAIN) %>%
-  mutate(Rating = case_when(
-    Control_Score > 3.499 ~ "Ineffective",
-    Control_Score > 2.499 & Control_Score <= 3.499 ~ "Marginally Effective",
-    Control_Score >= 1.5 & Control_Score <= 2.499 ~ "Generally Effective",
-    Control_Score < 1.5 ~ "Highly Effective"
-  )
-  )
+central.control.scores <- lob.control.results[1:2, ]
